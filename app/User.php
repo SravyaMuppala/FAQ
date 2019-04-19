b1adba3132dbecf9b81d914e4d0934af0d98ff12
sravya <saisravyam@gmail.com>
2019-04-19 16:44:03 -0400

    Added user profile migrate, model and updated the user seeder to create profiles.

--- app/User.php
+++ app/User.php
@@ -59,7 +59,11 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $casts = [
+    /*protected $casts = [
         'email_verified_at' => 'datetime',
-    ];
+    ];*/
+    public function profile()
+    {
+        return $this->hasOne('App\Profile');
+    }
 }
