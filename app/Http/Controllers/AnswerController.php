2c27497453315ce21a53c3cacf2f25ae28faa1d3
sravya <saisravyam@gmail.com>
2019-04-21 16:34:33 -0400

    Project Forms

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Answer;
+use App\Question;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -27,9 +28,11 @@ class AnswerController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create($question)
     {
-        //
+        $answer = new Answer;
+        $edit = FALSE;
+        return view('answerForm', ['answer' => $answer,'edit' => $edit, 'question' =>$question  ]);
     }
 
     /**
@@ -38,9 +41,25 @@ class AnswerController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, $question)
     {
-        //
+
+        $input = $request->validate([
+            'body' => 'required|min:5',
+        ], [
+
+            'body.required' => 'Body is required',
+            'body.min' => 'Body must be at least 5 characters',
+
+        ]);
+        $input = request()->all();
+        $question = Question::find($question);
+        $Answer = new Answer($input);
+        $Answer->user()->associate(Auth::user());
+        $Answer->question()->associate($question);
+        $Answer->save();
+
+        return redirect()->route('questions.show',['question_id' => $question->id])->with('message', 'Saved');
     }
 
     /**
@@ -63,9 +82,11 @@ class AnswerController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit($question,  $answer)
     {
-        //
+        $answer = Answer::find($answer);
+        $edit = TRUE;
+        return view('answerForm', ['answer' => $answer, 'edit' => $edit, 'question'=>$question ]);
     }
 
     /**
@@ -75,9 +96,23 @@ class AnswerController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, $question, $answer)
     {
-        //
+        $input = $request->validate([
+            'body' => 'required|min:5',
+        ], [
+
+            'body.required' => 'Body is required',
+            'body.min' => 'Body must be at least 5 characters',
+
+        ]);
+
+        $answer = Answer::find($answer);
+        $answer->body = $request->body;
+        $answer->save();
+
+        return redirect()->route('answers.show',['question_id' => $question, 'answer_id' => $answer])->with('message', 'Updated');
+
     }
 
     /**
@@ -86,8 +121,12 @@ class AnswerController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy($question, $answer)
     {
-        //
+        $answer = Answer::find($answer);
+
+        $answer->delete();
+        return redirect()->route('questions.show',['question_id' => $question])->with('message', 'Delete');
+
     }
 }
